fb6d5423c6389d94ba1cf744c8bf134a18d11aa7
Paul Davis <pcdavis@umass.edu>
2023-02-18 11:22:17 -0500

    SAS-3 Adding TailwindCSS dependencies and config settings

---
+++ mobile/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./assets/**/*.js",
+    "./components/**/*.js",
+    "./screens/**/*.js",
+    "./navigation/**/*.js",
+    "./models/**/*.js",
+    "./translations/**/*.js",
+    "./App.js",
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
