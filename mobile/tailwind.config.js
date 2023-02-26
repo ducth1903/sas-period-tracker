4c53fc4839418227a3a035352cc8bb2ea2f2beb1
Paul Davis <pcdavis@umass.edu>
2023-02-26 12:06:44 -0500

    feat: Color palette
    Adding Hi-Fi protype color palette colors to Tailwind theme
    
    feat: Color palette
    
    Adding Hi-Fi protype color palette colors to Tailwind theme

--- mobile/tailwind.config.js
+++ mobile/tailwind.config.js
@@ -10,6 +10,16 @@ module.exports = {
     "./App.js",
 ],
   theme: {
+    colors: {
+      'gray': '#D1D5DB',
+      'offwhite': '#F5F4FF',
+      'lightlavender': '#DDD6F6',
+      'lavender': '#C0B3F1',
+      'salmon': '#ff7f73',
+      'teal': '#00394E',
+      'turquoise': '#005C6A',
+      'seafoam': '#5B9F8F'
+    },
     extend: {},
   },
   plugins: [],
