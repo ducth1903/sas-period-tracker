7fbe4fbfab58bc5208bbe748f3c8d10131c96cc1
Anshul Saha <anshulsaha15@gmail.com>
2023-03-04 16:18:08 -0500

    made the basic layout for the intial screen

--- mobile/screens/Resource/ResourceHomeScreen.js
+++ mobile/screens/Resource/ResourceHomeScreen.js
@@ -8,10 +8,77 @@ import {
     SafeAreaView,  
     StatusBar,
     Image,
+    ScrollView,
 } from 'react-native';
 import RESOURCE_TEMPLATE from '../../models/ResourceModel';
 // import { AuthContext } from '../../navigation/AuthProvider'; 
 import { MARKDOWN_S3_URL } from '@env';
+
+const mockData = [
+    {
+        title: 'Saved',
+        data: [
+          {
+            key: '1',
+            text: 'How to Soothe Cramps'
+          },
+          {
+            key: '2',
+            text: 'See All'
+          },
+        ],
+    },
+    {
+        title: 'Recently Viewed',
+        data: [
+          {
+            key: '1',
+            text: 'What to Do on Your Period'
+          },
+          {
+            key: '2',
+            text: 'The First Period'
+          }
+        ],
+    },
+    {
+        title: 'Menstruation',
+        data: [
+            {
+                key: '1',
+                text: 'Period Basics'
+            },
+            {
+                key: '2',
+                text: "How-to's"
+            },
+            {
+                key: '3',
+                text: 'Health and Hygiene'
+            },
+            {
+                key: '4',
+                text: 'Taboos and Misconceptions'
+            },
+        ],
+    }
+]
+
+const RedListItem = ({ item }) => {
+    return (
+      <View style={styles.redBox}>
+        <Text style={styles.redBoxText}>{item.text}</Text>
+      </View>
+    );
+  };
+
+const PurpleListItem = ({ item }) => {
+    return (
+      <View style={styles.purpleBox}>
+        <Text style={styles.purpleBoxText}>{item.text}</Text>
+      </View>
+    );
+};
 
 const ResourceScreen = ({ navigation, props }) => {
     const resource_template = new RESOURCE_TEMPLATE();
@@ -74,25 +141,142 @@ const ResourceScreen = ({ navigation, props }) => {
 
     return (
         <SafeAreaView style={styles.container}>
-            <FlatList
-            data={resource_template.default_template}
-            renderItem={renderItem}
-            keyExtractor={item => item.id.toString()}
-            horizontal={false}
-            numColumns={2}
-            refreshing={refreshing}
-            onRefresh={onRefresh}
-            />
+            <ScrollView>
+                <View style={styles.inline}>
+                    <Text style={styles.headerText}>Education</Text>
+                    <Image source={require('../../assets/icons/search.png')} style={styles.headerSearchIcon}/>
+                </View>
+                <Text style={styles.subHeaderText}>Saved</Text>
+                <FlatList
+                    horizontal
+                    data={mockData[0].data}
+                    renderItem={({ item }) => <RedListItem item={item} />}
+                    showsHorizontalScrollIndicator={false}
+                    style={{marginBottom: -15}}
+                />
+                <Text style={styles.subHeaderText}>Recently Viewed</Text>
+                <FlatList
+                    horizontal
+                    data={mockData[1].data}
+                    renderItem={({ item }) => <RedListItem item={item} />}
+                    showsHorizontalScrollIndicator={false}
+                    style={{marginBottom: -15}}
+                />
+                <Text style={styles.subHeaderText}>Menstruation</Text>
+                <FlatList
+                    horizontal
+                    data={mockData[2].data}
+                    renderItem={({ item }) => <PurpleListItem item={item} />}
+                    showsHorizontalScrollIndicator={false}
+                    style={{marginBottom: -15}}
+                />
+                <Text style={styles.subHeaderText}>Menstruation</Text>
+                <FlatList
+                    horizontal
+                    data={mockData[2].data}
+                    renderItem={({ item }) => <PurpleListItem item={item} />}
+                    showsHorizontalScrollIndicator={false}
+                    style={{marginBottom: -15}}
+                />
+                <Text style={styles.subHeaderText}>Menstruation</Text>
+                <FlatList
+                    horizontal
+                    data={mockData[2].data}
+                    renderItem={({ item }) => <PurpleListItem item={item} />}
+                    showsHorizontalScrollIndicator={false}
+                    style={{marginBottom: -15}}
+                />
+                <Text style={styles.subHeaderText}>Menstruation</Text>
+                <FlatList
+                    horizontal
+                    data={mockData[2].data}
+                    renderItem={({ item }) => <PurpleListItem item={item} />}
+                    showsHorizontalScrollIndicator={false}
+                    // style={{marginBottom: -15}}
+                />
+            </ScrollView>
         </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
+    headerText: {
+        fontSize: 35,
+        fontWeight: "600",
+        color: "black",
+        textAlign: "center",
+        marginTop: 10,
+        marginLeft: 70,
+    },
+    headerSearchIcon: {
+        width: 30, 
+        height: 30, 
+        marginTop: 10, 
+        marginLeft: 50
+    },
+    inline: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+    },
+    redBox: {
+        margin: 20,
+        width: 250,
+        height: 170,
+        backgroundColor: '#FF7F73',
+        borderRadius: 15,
+        justifyContent: 'center',
+        alignItems: 'center',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.3,
+        shadowRadius: 3,
+        elevation: 5,
+        marginRight: 5,
+    },
+    redBoxText: {
+        color: 'white',
+        fontSize: 24,
+        fontWeight: '600',
+        textAlign: 'center',
+        width: '80%',
+    },
+    purpleBox: {
+        margin: 20,
+        width: 180,
+        height: 180,
+        backgroundColor: '#D9D1F7',
+        borderRadius: 15,
+        justifyContent: 'center',
+        alignItems: 'center',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.3,
+        shadowRadius: 3,
+        elevation: 5,
+        marginRight: 5,
+    },
+    purpleBoxText: {
+        color: 'black',
+        fontSize: 22,
+        fontWeight: '600',
+        textAlign: 'center',
+        width: '100%',
+    },
+    subHeaderText: {
+        fontSize: 24,
+        fontWeight: "600",
+        color: "black",
+        textAlign: "left",
+        marginTop: 20,
+        marginLeft: 20,
+        marginBottom: -5
+    },
     container: {
         flex: 1,
         justifyContent: 'flex-start',
         // alignContent: 'center',
-        backgroundColor: 'skyblue',
+        backgroundColor: '#FEFFF4',
         paddingTop: StatusBar.currentHeight,
     },
     menurow: {
@@ -120,7 +304,7 @@ const styles = StyleSheet.create({
         marginBottom: '10%',
         marginLeft: '10%',
         marginRight: '10%',
-        backgroundColor: 'white',
+        backgroundColor: 'blue',
         borderRadius: 20,
     },
     // scrollView: {
