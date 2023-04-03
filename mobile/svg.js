8c34189b08b032033b6272b323e42f5e4466d221
Paul Davis <paulcdavis135@gmail.com>
2023-04-03 13:50:45 -0400

    fix: Adding forgotten flow icons to svg.js

--- mobile/svg.js
+++ mobile/svg.js
@@ -2,9 +2,13 @@
 import FlowLightDefault from './assets/icons/blood_flow/light_default.svg';
 import FlowMediumDefault from './assets/icons/blood_flow/medium_default.svg';
 import FlowHeavyDefault from './assets/icons/blood_flow/heavy_default.svg';
+import FlowNoneDefault from './assets/icons/blood_flow/none_default.svg';
+import FlowNotSureDefault from './assets/icons/blood_flow/notsure_default.svg';
 import FlowLightSelected from './assets/icons/blood_flow/light_selected.svg';
 import FlowMediumSelected from './assets/icons/blood_flow/medium_selected.svg';
 import FlowHeavySelected from './assets/icons/blood_flow/heavy_selected.svg';
+import FlowNoneSelected from './assets/icons/blood_flow/none_selected.svg';
+import FlowNotSureSelected from './assets/icons/blood_flow/notsure_selected.svg';
 
 // Mood Icons
 import MoodExcitedDefault from './assets/icons/mood/excited_default.svg';
@@ -54,9 +58,13 @@ export {
   FlowLightDefault,
   FlowMediumDefault,
   FlowHeavyDefault,
+  FlowNoneDefault,
+  FlowNotSureDefault,
   FlowLightSelected,
   FlowMediumSelected,
   FlowHeavySelected,
+  FlowNoneSelected,
+  FlowNotSureSelected,
   MoodExcitedDefault,
   MoodHappyDefault,
   MoodSensitiveDefault,
