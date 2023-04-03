57ba5c0384275b2ca872a8d253885a19c7afb494
Paul Davis <paulcdavis135@gmail.com>
2023-04-02 22:13:45 -0400

    [SAS-21] feat: SVG Icons for flow, symptoms, and symptoms icons
    Also merged in main, which is why some additional changes are present

---
+++ mobile/svg.js
@@ -0,0 +1,108 @@
+// Blood Flow Icons
+import FlowLightDefault from './assets/icons/blood_flow/light_default.svg';
+import FlowMediumDefault from './assets/icons/blood_flow/medium_default.svg';
+import FlowHeavyDefault from './assets/icons/blood_flow/heavy_default.svg';
+import FlowNoneDefault from './assets/icons/blood_flow/none_default.svg';
+import FlowNotSureDefault from './assets/icons/blood_flow/notsure_default.svg';
+import FlowLightSelected from './assets/icons/blood_flow/light_selected.svg';
+import FlowMediumSelected from './assets/icons/blood_flow/medium_selected.svg';
+import FlowHeavySelected from './assets/icons/blood_flow/heavy_selected.svg';
+import FlowNoneSelected from './assets/icons/blood_flow/none_selected.svg';
+import FlowNotSureSelected from './assets/icons/blood_flow/notsure_selected.svg';
+
+// Mood Icons
+import MoodExcitedDefault from './assets/icons/mood/excited_default.svg';
+import MoodHappyDefault from './assets/icons/mood/happy_default.svg';
+import MoodSensitiveDefault from './assets/icons/mood/sensitive_default.svg';
+import MoodSadDefault from './assets/icons/mood/sad_default.svg';
+import MoodAnxiousDefault from './assets/icons/mood/anxious_default.svg';
+import MoodAngryDefault from './assets/icons/mood/angry_default.svg';
+import MoodExcitedSelected from './assets/icons/mood/excited_selected.svg';
+import MoodHappySelected from './assets/icons/mood/happy_selected.svg';
+import MoodSensitiveSelected from './assets/icons/mood/sensitive_selected.svg';
+import MoodSadSelected from './assets/icons/mood/sad_selected.svg';
+import MoodAnxiousSelected from './assets/icons/mood/anxious_selected.svg';
+import MoodAngrySelected from './assets/icons/mood/angry_selected.svg';
+
+// Symptom Icons
+import SymptomCravingsDefault from './assets/icons/symptoms/cravings_default.svg';
+import SymptomBackacheDefault from './assets/icons/symptoms/backache_default.svg';
+import SymptomCrampsDefault from './assets/icons/symptoms/cramps_default.svg';
+import SymptomBloatingDefault from './assets/icons/symptoms/bloating_default.svg';
+import SymptomTenderDefault from './assets/icons/symptoms/tender_default.svg';
+import SymptomHeadacheDefault from './assets/icons/symptoms/headache_default.svg';
+import SymptomFatigueDefault from './assets/icons/symptoms/fatigue_default.svg';
+import SymptomNauseaDefault from './assets/icons/symptoms/nausea_default.svg';
+import SymptomDischargeStringy from './assets/icons/symptoms/discharge_stringy_default.svg';
+import SymptomDischargeWatery from './assets/icons/symptoms/discharge_watery_default.svg';
+import SymptomDischargeTransparent from './assets/icons/symptoms/discharge_transparent_default.svg';
+import SymptomDischargeCreamy from './assets/icons/symptoms/discharge_creamy_default.svg';
+import SymptomDischargeClumpy from './assets/icons/symptoms/discharge_clumpy_default.svg';
+import SymptomDischargeSticky from './assets/icons/symptoms/discharge_sticky_default.svg';
+import SymptomCravingsSelected from './assets/icons/symptoms/cravings_selected.svg';
+import SymptomBackacheSelected from './assets/icons/symptoms/backache_selected.svg';
+import SymptomCrampsSelected from './assets/icons/symptoms/cramps_selected.svg';
+import SymptomBloatingSelected from './assets/icons/symptoms/bloating_selected.svg';
+import SymptomTenderSelected from './assets/icons/symptoms/tender_selected.svg';
+import SymptomHeadacheSelected from './assets/icons/symptoms/headache_selected.svg';
+import SymptomFatigueSelected from './assets/icons/symptoms/fatigue_selected.svg';
+import SymptomNauseaSelected from './assets/icons/symptoms/nausea_selected.svg';
+import SymptomDischargeStringySelected from './assets/icons/symptoms/discharge_stringy_selected.svg';
+import SymptomDischargeWaterySelected from './assets/icons/symptoms/discharge_watery_selected.svg';
+import SymptomDischargeTransparentSelected from './assets/icons/symptoms/discharge_transparent_selected.svg';
+import SymptomDischargeCreamySelected from './assets/icons/symptoms/discharge_creamy_selected.svg';
+import SymptomDischargeClumpySelected from './assets/icons/symptoms/discharge_clumpy_selected.svg';
+import SymptomDischargeStickySelected from './assets/icons/symptoms/discharge_sticky_selected.svg';
+
+export {
+  FlowLightDefault,
+  FlowMediumDefault,
+  FlowHeavyDefault,
+  FlowNoneDefault,
+  FlowNotSureDefault,
+  FlowLightSelected,
+  FlowMediumSelected,
+  FlowHeavySelected,
+  FlowNoneSelected,
+  FlowNotSureSelected,
+  MoodExcitedDefault,
+  MoodHappyDefault,
+  MoodSensitiveDefault,
+  MoodSadDefault,
+  MoodAnxiousDefault,
+  MoodAngryDefault,
+  MoodExcitedSelected,
+  MoodHappySelected,
+  MoodSensitiveSelected,
+  MoodSadSelected,
+  MoodAnxiousSelected,
+  MoodAngrySelected,
+  SymptomCravingsDefault,
+  SymptomBackacheDefault,
+  SymptomCrampsDefault,
+  SymptomBloatingDefault,
+  SymptomTenderDefault,
+  SymptomHeadacheDefault,
+  SymptomFatigueDefault,
+  SymptomNauseaDefault,
+  SymptomDischargeStringy,
+  SymptomDischargeWatery,
+  SymptomDischargeTransparent,
+  SymptomDischargeCreamy,
+  SymptomDischargeClumpy,
+  SymptomDischargeSticky,
+  SymptomCravingsSelected,
+  SymptomBackacheSelected,
+  SymptomCrampsSelected,
+  SymptomBloatingSelected,
+  SymptomTenderSelected,
+  SymptomHeadacheSelected,
+  SymptomFatigueSelected,
+  SymptomNauseaSelected,
+  SymptomDischargeStringySelected,
+  SymptomDischargeWaterySelected,
+  SymptomDischargeTransparentSelected,
+  SymptomDischargeCreamySelected,
+  SymptomDischargeClumpySelected,
+  SymptomDischargeStickySelected,
+};
