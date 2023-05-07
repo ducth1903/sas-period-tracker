7a5456e4a43d11e8f083d094de46be1ffb7205ba
Nhan0504 <ttn0504ltt@gmail.com>
2023-05-07 12:33:40 -0400

    Move connecting to server to the services

--- server/api/v0/services/get_aws.py
+++ server/api/v0/services/get_aws.py
@@ -1,4 +1,7 @@
 import boto3
+import os
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -14,3 +17,22 @@ class SasAws:
         self.s3 = boto3.client("s3", region_name="us-east-1")
         self.S3_BUCKET = "s3-sas-period-tracker"
         # S3_PROFILE_IMAGE_PATH = 'https://sas-period-tracker.s3.amazonaws.com/profile-images/'
+
+        # AWS RDS
+        db_user = os.getenv("AWS_RDS_RESOURCE_USER")
+        db_password = os.getenv("AWS_RDS_RESOURCE_PASSWORD")
+        db_host = os.getenv("AWS_RDS_RESOURCE_HOST")
+        db_port = os.getenv("AWS_RDS_RESOURCE_PORT")
+        db_name = os.getenv("AWS_RDS_RESOURCE_NAME")
+
+        # create the URL string
+        url = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+
+        # create the SQLAlchemy engine
+        engine = create_engine(url)
+
+        # Create a Session class
+        Session = sessionmaker(bind=engine)
+        # Create the actual session
+        self.rds = Session()
+
