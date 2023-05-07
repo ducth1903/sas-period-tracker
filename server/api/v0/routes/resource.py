471038b4b03d0f9960c649cbc7bd63df2f703221
Nhan0504 <ttn0504ltt@gmail.com>
2023-05-07 12:34:05 -0400

    Use sas_aws.rds

--- server/api/v0/routes/resource.py
+++ server/api/v0/routes/resource.py
@@ -1,23 +1,12 @@
 from flask import Blueprint, request, current_app, jsonify, json
-from dotenv import load_dotenv
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-import os
 
 from ..services.get_aws import SasAws
 from ..utils import response
 
-load_dotenv()
-
 resource_api = Blueprint('resource_api', __name__)
 sas_aws = SasAws()
-
-db_user = os.getenv("AWS_RDS_RESOURCE_USER")
-db_password = os.getenv("AWS_RDS_RESOURCE_PASSWORD")
-db_host = os.getenv("AWS_RDS_RESOURCE_HOST")
-db_port = os.getenv("AWS_RDS_RESOURCE_PORT")
-db_name = os.getenv("AWS_RDS_RESOURCE_NAME")
 
 # Create a base class for declarative ORM models
 Base = declarative_base()
@@ -41,21 +30,10 @@ class Resource(Base):
         self.category = category
         self.timestamp = timestamp
 
-# create the URL string
-url = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
-
-# create the SQLAlchemy engine
-engine = create_engine(url)
-
-# Create a Session class
-Session = sessionmaker(bind=engine)
-# Create the actual session
-session = Session()
-
 # GET
 @resource_api.route('/resources/<id>')
 def resources_get_by_id(id):
-    resp = session.query(Resource).filter(Resource.id == id).first()
+    resp = sas_aws.rds.query(Resource).filter(Resource.id == id).first()
     current_app.logger.info(f"[GET] response: {resp}")
         # resource is not in metadata table
     if not resp:
@@ -79,8 +57,8 @@ def resources_post_by_id(id):
             "category": rec["category"],
             "timestamp": rec["timestamp"]
         }
-        session.add(resource_obj)
-        session.commit()
+        sas_aws.rds.add(resource_obj)
+        sas_aws.rds.commit()
 
     return response.ok_json_response()
 
@@ -92,32 +70,32 @@ def resources_put_by_id(id):
     current_app.logger.info("[PUT] received data: ", received_json_data)
 
     # Get the resource
-    resource = session.query(Resource).filter(Resource.id == id).first()
+    resource = sas_aws.rds.query(Resource).filter(Resource.id == id).first()
 
     resource.title = received_json_data['name']
     resource.s3_url = received_json_data['s3_url']
     resource.author = received_json_data['author']
     resource.category = received_json_data['category']
     resource.timestamp = received_json_data['timestamp']
-    session.commit()
+    sas_aws.rds.commit()
 
     return received_json_data
 
 # DELETE
 @resource_api.route('/resources/<id>')
 def resources_delete_by_id(id):
-    resource = session.query(Resource).filter(Resource.id == id).first()
+    resource = sas_aws.rds.query(Resource).filter(Resource.id == id).first()
 
     if not resource:
         return response.error_json_response(id)
     
-    session.delete(resource)
-    session.commit()
+    sas_aws.rds.delete(resource)
+    sas_aws.rds.commit()
     return response.ok_json_response
 
 @resource_api.route('/resources/<category>')
 def resources_get_by_category(category):
-    resp = session.query(Resource).filter(Resource.category == category).all()
+    resp = sas_aws.rds.query(Resource).filter(Resource.category == category).all()
     current_app.logger.info(f"[GET] response: {resp}")
     # resource is not in metadata table
     if len(resp) == 0:
@@ -131,7 +109,7 @@ def resources_get_by_category(category):
 
 @resource_api.route('/resources/<author>')
 def resources_get_by_author(author):
-    resp = session.query(Resource).filter(Resource.category == author).all()
+    resp = sas_aws.rds.query(Resource).filter(Resource.category == author).all()
     current_app.logger.info(f"[GET] response: {resp}")
     # resource is not in metadata table
     if len(resp) == 0:
