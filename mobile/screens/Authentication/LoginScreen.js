f3da29ab152954fee33330fef09466926e3822aa
Paul Davis <paulcdavis135@gmail.com>
2023-07-05 16:42:23 -0400

    [SAS-57] login screen polishing

--- mobile/screens/Authentication/LoginScreen.js
+++ mobile/screens/Authentication/LoginScreen.js
@@ -3,156 +3,160 @@ import {
   StyleSheet,
   Text,
   View,
+  ScrollView,
   TouchableOpacity,
   Dimensions,
   Image,
+  Pressable,
 } from "react-native";
 import LanguagePicker from "../../components/LanguagePicker";
-// import * as Animatable from 'react-native-animatable';
 
 import { AuthContext } from "../../navigation/AuthProvider";
+import { SettingsContext } from "../../navigation/SettingsProvider";
 import FormInput from "../../components/FormInput";
 import FormButton from "../../components/FormButton";
 import SignInWithGoogle from "../../navigation/SignInWithGoogle";
-// import SocialButton from '../../components/SocialButton';
 import i18n from "../../translations/i18n";
 
 const LoginScreen = ({ navigation }) => {
   const { login, loginWithGoogle, authError, setAuthError } = useContext(AuthContext);
+  const { selectedSettingsLanguage } = useContext(SettingsContext);
   const [hiddenPassword, setHiddenPassword] = useState(true);
 
-  const [isOpen, setIsOpen] = useState(false);
+  const [languagePickerOpen, setLanguagePickerOpen] = useState(false);
   const [selectedLanguage, setSelectedLanguage] = useState("English");
 
   const [inUserData, setInUserData] = useState({
     email: "",
-    password: "",
-    // isValidUser: true,
-    // isValidPassword: true
+    password: ""
   });
 
   useEffect(() => {
     setAuthError("");
   }, []);
 
-  handleEmail = (inEmail) => {
+  const handleEmail = (inEmail) => {
     setInUserData({
       ...inUserData,
       email: inEmail,
     });
   };
-  handlePassword = (inPassword) => {
+  const handlePassword = (inPassword) => {
     setInUserData({
       ...inUserData,
       password: inPassword,
     });
   };
-  login_with_email = (inEmail, inPassword) => {
+  const login_with_email = (inEmail, inPassword) => {
     login(inEmail, inPassword);
   };
-  handleEyeClicked = () => {
+  const handleEyeClicked = () => {
     setHiddenPassword(!hiddenPassword);
   };
 
+  const errorToString = (error) => {
+    if (error.toLowerCase().includes("auth/invalid-email")){
+      return i18n.t('authentication.invalidEmail');
+    }
+    else if (error.toLowerCase().includes("auth/wrong-password")) {
+      return i18n.t('authentication.invalidPassword');
+    }
+    else {
+      console.log(`[LoginScreen] errorToString: error = ${error}`)
+      return i18n.t('authentication.unrecognizedError');
+    }
+  }
+
   return (
-    <View style={{ flex: 1 }}>
-      <View style={styles.header}>
-        <Image
-          source={require("../../assets/revised-logo.png")}
-          style={styles.logo}
-        />
-      </View>
-
-      <View style={styles.viewWelcome}>
-        <Text style={styles.welcomeText}>{i18n.t('authentication.welcomeBack')}</Text>
-      </View>
-
-      <View style={styles.footer}>
-        <View style={styles.formInputContainer}>
-          <LanguagePicker />
-          <FormInput
-            labelValue="Email"
-            placeholderText={i18n.t("authentication.emailAddress")}
-            iconType="user"
-            color="black"
-            keyboardType="email-address"
-            value={inUserData.email}
-            onChangeText={handleEmail}
+    <ScrollView contentContainerStyle={{ backgroundColor: '#FEFFF4', paddingTop: 20 }}>
+      <View style={{ flex: 1 }}>
+        <View style={styles.header}>
+          <Image
+            source={require("../../assets/revised-logo.png")}
+            style={styles.logo}
           />
-          <FormInput
-            labelValue="Password"
-            placeholderText={i18n.t("authentication.password")}
-            iconType="lock"
-            color="black"
-            //isPassword={true}
-            secureTextEntry={hiddenPassword}
-            value={inUserData.password}
-            onChangeText={handlePassword}
-          //onPressEyeBtn={handleEyeClicked}
-          />
-          <View>
-            <FormButton
-              btnTitle={i18n.t("authentication.signIn")}
-              isHighlight={true}
-              onPress={() =>
-                login_with_email(inUserData.email, inUserData.password)
-              }
-            />
-          </View>
         </View>
 
-        {authError ? (
-          // <Animatable.View animation="fadeInLeft" duration={500}>
-          <View>
-            <Text style={[styles.centerItems, styles.errorMsg]}>
-              {authError}
-            </Text>
-          </View>
-        ) : null}
+        <View style={styles.viewWelcome}>
+          <Text style={styles.welcomeText}>{i18n.t('authentication.welcomeBack')}</Text>
+        </View>
 
-        {/* <SocialButton 
-                    btnTitle="Sign In With Google"
-                    btnType="google"
-                    btnColor="#de4d41" 
-                    bgColor="#f5e5ea"
-                    onPress={ () => console.log("gmail") } /> */}
-        {/* <SocialButton 
-                    btnTitle="Sign In With Facebook"
-                    btnType="facebook-square"
-                    btnColor="#4867aa" 
-                    bgColor="#e6eaf4"
-                    onPress={ () => console.log("facebook") } /> */}
-        <TouchableOpacity
-          onPress={() => {
-            navigation.navigate("ForgotPasswordScreen");
-          }}
-          style={styles.centerItems}
-        >
-          <Text style={[styles.textStyle, styles.bottomItems]}>
-            {i18n.t("authentication.forgotPassword")}
-          </Text>
-        </TouchableOpacity>
-
-        <View style={styles.centerItems}>
-          <Text style={{ fontSize: 16 }}>
-            {i18n.t("authentication.noAccount")}
-            <Text
-              style={styles.signUpStyle}
-              onPress={() => {
-                navigation.navigate("SignupScreen");
-              }}
+        <View style={styles.body}>
+          <View style={styles.formInputContainer}>
+            <Pressable
+              style={styles.languagePickerContainer}
+              onPress={() => {setLanguagePickerOpen(!languagePickerOpen)}}
             >
-              {" "}
-              {i18n.t("authentication.signUp")}
-            </Text>
-          </Text>
-        </View>
+              <LanguagePicker />
+            </Pressable>
+            <FormInput
+              labelValue="Email"
+              placeholderText={i18n.t("authentication.emailAddress")}
+              iconType="user"
+              color="black"
+              keyboardType="email-address"
+              value={inUserData.email}
+              onChangeText={handleEmail}
+            />
+            <FormInput
+              labelValue="Password"
+              placeholderText={i18n.t("authentication.password")}
+              iconType="lock"
+              color="black"
+              secureTextEntry={hiddenPassword}
+              value={inUserData.password}
+              onChangeText={handlePassword}
+            />
+            <View>
+              <FormButton
+                btnTitle={i18n.t("authentication.signIn")}
+                isHighlight={true}
+                onPress={() =>
+                  login_with_email(inUserData.email, inUserData.password)
+                }
+              />
+            </View>
+            {authError ? (
+              <View>
+                <Text style={[styles.centerItems, styles.errorMsg]}>
+                  {errorToString(authError)}
+                </Text>
+              </View>
+            ) : null}
+          </View>
 
-        <View style={styles.formInputContainer}>
-          <SignInWithGoogle successFn={loginWithGoogle} />
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate("ForgotPasswordScreen");
+            }}
+            style={styles.centerItems}
+          >
+            <Text style={[styles.textStyle, styles.bottomItems]}>
+              {i18n.t("authentication.forgotPassword")}
+            </Text>
+          </TouchableOpacity>
+
+          <View style={styles.centerItems}>
+            <Text style={{ fontSize: 16 }}>
+              {i18n.t("authentication.noAccount")}
+              <Text
+                style={styles.signUpStyle}
+                onPress={() => {
+                  navigation.navigate("SignupScreen");
+                }}
+              >
+                {" "}
+                {i18n.t("authentication.signUp")}
+              </Text>
+            </Text>
+          </View>
+
+          <View style={styles.formInputContainer}>
+            <SignInWithGoogle successFn={loginWithGoogle} />
+          </View>
         </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
@@ -164,21 +168,9 @@ const styles = StyleSheet.create({
     justifyContent: "flex-start",
     alignItems: "stretch",
   },
-  // footer: {
-  //     flex: 1,
-  //     backgroundColor: '#F6F6F6',
-  //     alignItems: 'center',
-  //     // justifyContent: 'flex-start',
-  //     borderTopLeftRadius: 30,
-  //     borderTopRightRadius: 30,
-  //     paddingVertical: 50,
-  //     paddingHorizontal: 30,
-
-  //     flexDirection: 'row',
-  //     flexWrap: 'wrap',
-  //     // alignItems: 'flex-start'
-  // },
-
+  body: {
+    marginTop: 20,
+  },
   centerItems: {
     alignItems: "center",
     justifyContent: "center",
@@ -186,13 +178,8 @@ const styles = StyleSheet.create({
   },
   textStyle: {
     fontSize: 16,
-    //color: "#AC3632",
   },
-  // textUnderlineStyle: {
-  //   textDecorationLine: "underline",
-  // },
   bottomItems: {
-    // flex: 1,
     justifyContent: "flex-end",
   },
   errorMsg: {
@@ -217,8 +204,7 @@ const styles = StyleSheet.create({
   welcomeText: {
     textAlign: "center",
     fontWeight: 700,
-    fontSize: 32,
-    lineHeight: 43,
+    fontSize: 32
   },
   footer: {
     flex: 5,
@@ -227,13 +213,12 @@ const styles = StyleSheet.create({
   },
   signIn: {
     paddingTop: 85,
-    //paddingBottom: 30
   },
   languagePickerContainer: {
     paddingLeft: 25,
     paddingRight: 25,
     margin: 8,
-    zIndex: 1,
+    zIndex: 1
   },
   formInputContainer: {
     paddingLeft: 25,
